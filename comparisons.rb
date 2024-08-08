5ef1d839ed19d8579ad30b2c4b1cb699c0838517
Jonathan Atkins <jonathanjatkins89@gmail.com>
2024-08-08 12:52:19 -0700

    Answeing Questions for Comparisons

--- comparisons.rb
+++ comparisons.rb
@@ -18,30 +18,43 @@ string_teachers = "4"
 numberDogs = 0
 
 # EXAMPLE: print the result of the comparison: is number_seachers greater than number_students?
-puts "Is number_teachers greater than number_students?", number_teachers > number_students
+#puts "Is number_teachers greater than number_students?", number_teachers > number_students
 # this should print: "Is numberTeachers greater than numberStudents?" false
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
 # this should print: true
 
+puts number_teachers < number_students
+
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
 # this should print: false
+
+puts number_teachers == string_teachers
 
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
 # this should print: true
 
+puts number_teachers != number_students
+
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
 # this should print: true
+
+puts number_students >= 20
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
 # this should print: false
 
+puts number_students >= 21
+
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
 # this should print: true
+
+puts number_students <= 20
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
 # this should print: true
 
+puts number_students <= 21
 
 #-------------------
 # PART 2: Articulating what you are doing
@@ -52,23 +65,33 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # Be as technically precise as possible, but don't just copy and paste a definition from the readings.
 # Make sure YOU can explain it that way!
 
-puts 4 < 9
+#puts 4 < 9
 #YOU DO: Explain.
 
-books = 3
-puts 4 < books
+# Asking Boolean statement if 4 is less than 9
+
+#books = 3
+#puts 4 < books
 # YOU DO: Explain.
+
+#This is assigning a variable name called books with the value of 3.
+#Then we are asking a boolean if 4 is less than the value of books which in this case is False
 
 friends = 6
 siblings = 2
-puts friends > siblings
+#puts friends > siblings
 # YOU DO: Explain.
+
+#We are assinging two variables. friends and siblings. Friends has variable of 6 and siblings 2.
+#We are asking a boolean statement if friends value is greater than siblings. This should render true
 
 attendees = 9
 meals = 8
-puts attendees != meals
+#puts attendees != meals
 # YOU DO: Explain.
 
+#Assigning two variables
+#Asking if attendess does not equal meals and the answer is true
 
 #-------------------
 # PART 3: Logical Operators
@@ -79,9 +102,22 @@ finished_homework = false
 
 # EXAMPLE:
 # Determine if the user is hungry and has completed their homework
-puts is_hungry && finished_homework
+
+if is_hungry  == true && finished_homework == true
+    puts true
+else
+    puts false
+end
+
+
 # Determine if the user is hungry or has completed their homework
-puts is_hungry || finished_homework
+#puts is_hungry || finished_homework
+
+if is_hungry == true || finished_homework == true
+    puts true
+else
+    puts false
+end
 
 loves_to_play = true
 loves_dog_park = false
@@ -91,15 +127,42 @@ age = 1
 # YOU DO:
 # Determine if the dog loves to play and loves treats
 
+if loves_to_play == true && loves_treats == true
+    puts true
+else
+    puts false
+end
 
 # Determine if the dog loves to play and loves the dog park
 
+if loves_to_play== true && loves_dog_park== true
+    puts true
+else
+    puts false
+end
 
 # Determine if the dog loves to play or loves the dog park
 
+if loves_to_play == true || loves_dog_park == true
+    puts true
+else
+    puts false 
+end
+
 
 # Determine if the dog loves to play and is a puppy
+
+if loves_to_play && age < 2
+    puts true
+else
+    puts false
+end
+
+
 # Assume that a dog is a puppy if it is less than 2 years old.
+
 
 # What did your final line of code evaluate to? Why do you think that is? Explain.
 # ANSWER:
+
+#it evaluated to true because loves to play is true and age is less than 2.
